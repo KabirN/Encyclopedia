0f874a70cbf23edcc42b9e3e1a74a25fe75f75fa
KabirN <oporajita955@gmil.com>
2022-04-17 21:57:38 +0600

    updating urls.py

--- wiki/settings.py
+++ wiki/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
